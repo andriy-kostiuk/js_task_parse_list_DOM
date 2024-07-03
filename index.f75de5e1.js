405bbb51c777815a59ff622e143d76b276e9a40b
Andrii <andriykostiuk1@gmail.com>
2024-07-03 20:28:38 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,26 @@
+"use strict";
+const pageList = document.querySelector("ul");
+const sortList = (list)=>{
+    const copyChildren = [
+        ...list.children
+    ];
+    copyChildren.sort((first, second)=>{
+        const getNormalizeSalary = (item)=>{
+            return +item.dataset.salary.slice(1).split(",").join("");
+        };
+        return getNormalizeSalary(second) - getNormalizeSalary(first);
+    });
+    copyChildren.forEach((employee)=>list.append(employee));
+};
+const getEmployees = (list)=>[
+        ...list.children
+    ].map((employee)=>({
+            name: employee.innerText,
+            position: employee.dataset.position,
+            age: employee.dataset.age,
+            salary: employee.dataset.salary
+        }));
+sortList(pageList);
+getEmployees(pageList);
+
+//# sourceMappingURL=index.f75de5e1.js.map
